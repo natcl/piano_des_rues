373fcefe9eb6d2fed21dc2eaf422e6807646947a
Nathanaël Lécaudé <nat@studioimaginaire.com>
2013-06-04 11:11:55 -0400

    Code cleanup

--- python/serial_2811.py
+++ python/serial_2811.py
@@ -1,6 +1,7 @@
 import serial
 import time
 import sys
+import random
 
 if sys.platform == 'darwin':
     serial_port = '/dev/tty.usbmodem11571'
@@ -9,33 +10,7 @@ elif sys.platform == 'linux2':
 
 numleds = 60
 teensy = serial.Serial(serial_port, 115200)
-led_list = []
-
-for x in range(numleds):
-    led_list.append(255)
-    led_list.append(255)
-    led_list.append(0)
-
-teensy.write(bytearray(led_list))
-time.sleep(2)
-
-led_list = []
-for x in range(numleds):
-    led_list.append(0)
-    led_list.append(255)
-    led_list.append(0)
-
-teensy.write(bytearray(led_list))
-time.sleep(2)
-
-led_list = []
-for x in range(numleds):
-    led_list.append(0)
-    led_list.append(0)
-    led_list.append(255)
-
-teensy.write(bytearray(led_list))
-time.sleep(2)
+led_list = [0 for x in range(numleds*3)]
 
 
 def setPixel(pixel, r, g, b):
@@ -48,11 +23,34 @@ def update():
     teensy.write(bytearray(led_list))
 
 for x in range(numleds):
+    setPixel(x, 255, 255, 0)
+update()
+time.sleep(2)
+
+for x in range(numleds):
+    setPixel(x, 0, 255, 0)
+update()
+time.sleep(2)
+
+for x in range(numleds):
+    setPixel(x, 0, 0, 255)
+update()
+time.sleep(2)
+
+for x in range(numleds):
     if x % 2:
         setPixel(x, 255, 255, 255)
 update()
 time.sleep(2)
+
+for x in range(numleds):
+    setPixel(x, random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+update()
+time.sleep(2)
+
 for x in range(numleds):
     setPixel(x, 0, 0, 0)
 update()
+
+time.sleep(0.1)
 teensy.close()
