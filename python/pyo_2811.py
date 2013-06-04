a4b24af9c5a81f9937ae44079a35966d9cfdb02a
Nathanaël Lécaudé <nat@studioimaginaire.com>
2013-06-04 03:13:41 +0000

    Modified for the pi

--- python/pyo_2811.py
+++ python/pyo_2811.py
@@ -2,24 +2,29 @@ from pyo import *
 import serial
 import time
 
-arduino = serial.Serial('/dev/tty.usbmodem411', 9600)
-time.sleep(2)
-def setPixel(pixel, r, g, b):
-        arduino.write(chr(pixel)+chr(r)+chr(g)+chr(b))
+teensy = serial.Serial('/dev/ttyACM0', 115200)
+led_list = []
+numleds = 60
 
 s = Server(duplex=0).boot()
 a = SuperSaw(freq=[100,2000], detune=0.6, bal=0.7, mul=0.5).out()
+#l = Linseg([(0,20),(2,15000)], loop=True)
+#a = Sine(freq=[l,1000], mul=1).out()
+#l.play()
+
 def tolist(x):
-    for pixel in x[0]:
-        #print pixel[0],255-pixel[1]
-        setPixel(pixel[0],255-pixel[1],0,0)
-    for pixel in x[1]:
-        setPixel(pixel[0],0,0,255-pixel[1])
+    global led_list
+    for led in range(numleds):
+        #red is left channel
+        led_list.append(255-x[0][led][1])
+        led_list.append(0)
+        #green is right channel
+        led_list.append(255-x[1][led][1])
+    teensy.write(bytearray(led_list))
+    led_list = []
 
 spec = Spectrum(a, size=1024, function=tolist)
-spec.setWidth(60)
+spec.setWidth(numleds)
 spec.setHeight(255)
 s.start()
 
-while 1:
-	pass
--- python/serial_2811.py
+++ python/serial_2811.py
@@ -3,8 +3,7 @@ import time
 #from random import randint
 
 numleds = 60
-teensy = serial.Serial('/dev/tty.usbmodem11571', 115200)
-time.sleep(2)
+teensy = serial.Serial('/dev/ttyACM0',115200)
 
 led_list = []
 
@@ -46,6 +45,8 @@ for x in range(numleds):
     if x % 2:
         setPixel(x,255,255,255)
 update()
-
-time.sleep(10)
+time.sleep(2)
+for x in range(numleds):
+    setPixel(x,0,0,0)
+update()
 teensy.close()
